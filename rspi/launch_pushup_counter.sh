a7f87544ecdbd5a2eba623131c5db71c99f34bb1
cook <cook@cook-MBP.local>
2022-05-14 11:01:22 +0800

    fix shell exist file

--- rspi/launch_pushup_counter.sh
+++ rspi/launch_pushup_counter.sh
@@ -11,7 +11,7 @@ echo "launch pushup counter - current_dir is $current_dir" >> $log_file
 source $current_dir/dest/env.sh
 
 # 1. kill previous counter process if exist
-if [ -f "$pid_path"] ; then
+if test -f "$pid_path"; then
   echo "begin kill previous counter process#$prev_pid" >> $log_file
   prev_pid="`cat $pid_path`"
   kill_msg=`kill -9 $prev_pid`
